27bb03629aee05b53df9cc2a53c59ede910fc65c
Akshay <akshay@Akshays-MacBook-Air.local>
2022-04-09 18:46:12 +0530

    Authorization using bcrypt

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 //jshint esversion:6
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5")
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(
@@ -39,35 +40,38 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  const user = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-
-  user.save(function (err) {
-    if (!err) {
-      res.render("secrets");
-    } else {
-      console.log(err);
-    }
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const user = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    user.save(function (err) {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    });
   });
 });
 
-app.post("/login", function(req, res){
-    const username = req.body.username
-    const password = md5(req.body.password)
-    User.findOne({email: username}, function(err, foundUser){
-        if(err){
-            console.log(err);
-        }else{
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets")
-                }
-            }
-        }
-    })
-})
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if(foundUser){
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
+      }
+    }
+  });
+});
 
 app.listen(3000, function () {
   console.log("Your server listening at port: 3000");
